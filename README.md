4c1e2726972b3473092413b9df5c08f2c670efc3
Bradley Chatha <sealabjaster@gmail.com>
2021-08-30 18:43:12 +0100

    Update README, fix infinite loop

--- README.md
+++ README.md
@@ -1,3 +1,80 @@
+# Overview
+
+`libsdlang` is a header-only C library used to parse and (todo) emit [Sdlang](https://sdlang.org/) files.
+
+This library comes bundled with the `ds` (data structure) module from the [stb](https://github.com/nothings/stb) project for convenience.
+
+# Installing
+
+First, grab the `include/libsdlang.h` and `include/stb_ds.h` headers.
+
+If you already use `stb_ds` then you won't need the version bundled in this repo, it's included just to make this library even easier to include.
+
+Then in **one** .C file add:
+
+```c
+#define SDLANG_IMPLEMENTATION
+#include <libsdlang.h> // or "libsdlang.h"
+#define STB_DS_IMPLEMENTATION
+#include <stb_ds.h>
+```
+
+Then just `#include` those two headers in any file you need to use this library in.
+
+# Usage
+
+Please note that the arrays in this library are from the `stb_ds` library, so please refer to that library for
+usage details.
+
+In short:
+
+* Create an `SdlangCharStream` containing the SDL to parse.
+* Call `sdlangParseCharStream`
+* ???
+* Yes
+
+In code:
+
+```c
+const char* sdl = 
+"message \"Hello, world!\" {\n           \
+    sent 2021/08/30 18:00:00 by=`brad`\n \
+}                                        \
+";
+
+SdlangCharStream stream = { sdl, strlen(sdl) };
+SdlangError error;                      // Set if an error occurs.
+SdlangCharSlice errorLine, errorSlice;  // errorLine = The line an error happened, errorSlice = The text that (probably) caused the error.
+SdlangTag tag = {};                     // The root tag.
+
+const bool parsed = sdlangParseCharStream(stream, &tag, &error, &errorLine, &errorSlice);
+if(!parsed)
+    assert(0);
+
+size_t i;
+for(i = 0; i < arrlen(tag.children); i++) // arrlen being from stb_ds
+{
+    SdlangTag child = tag.children[i];
+    if(strncmp("message", child.name.ptr, child.name.length) == 0)
+    {
+        if(child.values[0].type != SDLANG_VALUE_TYPE_STRING)
+            assert(0);
+
+        SdlangCharSlice msg = child.values[0].stringValue;
+        SdlangDateTime sent = child.children[0].values[0].dateTimeValue;
+        SdlangCharSlice by = child.children[0].attributes[0].value.stringValue;
+
+        printf(
+            "Message: '%.*s'\nSent by: %.*s\nOn: %d/%d/%d %d:%d:%d\n",
+            (int)msg.length, msg.ptr,
+            (int)by.length, by.ptr,
+            (int)sent.date.year, sent.date.month, sent.date.day,
+            sent.time.hours, sent.time.minutes, sent.time.seconds
+        );
+    }
+}
+```
+
 # Limitations
 
 * Base64 encoded values are not supported and won't parse.
--- include/libsdlang.h
+++ include/libsdlang.h
@@ -1,4 +1,18 @@
-﻿#pragma once
+﻿/*
+============ LICENSE ============
+Copyright 2021 (c) Bradley Chatha
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+#pragma once
 
 #include <assert.h>
 #include <stdbool.h>
@@ -759,26 +773,32 @@ void sdlangParserNext(SdlangParser* parser, SdlangError* error, SdlangCharSlice*
                 return;
             }
 
-            _someNumeric(
-                parser, 
-                &parser->front.intValue, 
-                &parser->front.floatValue, 
-                &parser->front.timeSpanValue, 
-                &parser->front.dateValue, 
-                &parser->front.dateTimeValue, 
-                &parser->front.type, 
-                error
-            );
-            if(*error)
+            if(ch == '-' || (ch >= '0' && ch <= '9'))
             {
-                *errorLine = sdlangCharStreamGetLine(&parser->stream, parser->stream.cursor);
-                return;
+                _someNumeric(
+                    parser, 
+                    &parser->front.intValue, 
+                    &parser->front.floatValue, 
+                    &parser->front.timeSpanValue, 
+                    &parser->front.dateValue, 
+                    &parser->front.dateTimeValue, 
+                    &parser->front.type, 
+                    error
+                );
+                if(*error)
+                {
+                    *errorLine = sdlangCharStreamGetLine(&parser->stream, parser->stream.cursor);
+                    return;
+                }
+                else
+                {
+                    parser->front.end = parser->stream.cursor;
+                    return;
+                }
             }
-            else
-            {
-                parser->front.end = parser->stream.cursor;
-                return;
-            }
+
+            *error = SDLANG_ERROR_UNEXPECTED_CHARACTER;
+            *errorLine = sdlangCharStreamGetLine(&parser->stream, parser->stream.cursor);
             break;
 
         default: assert(false);
